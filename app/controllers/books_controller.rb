f9842ca2264238b87b2f686edf676e479b56008c
hayashinoshun <s.y1014torimaru@gmail.com>
2022-07-29 16:01:53 +0900

    edit(編集機能実装完了)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,19 @@ class BooksController < ApplicationController
         end
     end
 
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+            redirect_to @book
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
     private
         def book_params
             params.require(:book).permit(:title, :author)
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ get "/books", to: "books#index"
 get "/books/new", to: "books#new", as: :new_book
 post "/books", to: "books#create"
 get "/books/:id", to: "books#show", as: :book
+get "/books/:id/edit", to: "books#edit", as: :edit_book
+patch "/books/:id", to: "books#update"
 end
