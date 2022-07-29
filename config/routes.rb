cf22a12be18710065e6da31ae4ea1d14eb0989e1
hayashinoshun <s.y1014torimaru@gmail.com>
2022-07-29 15:05:35 +0900

    users, books, help遷移作成 booksにて新規追加機能の作成完了 routhにてCRUD機能作成途中  WIP 現段段階では、編集画面の作成と実行環境の作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 get "/help", to: "home#help"
+get "/books", to: "books#index"
+get "/books/new", to: "books#new", as: :new_book
+post "/books", to: "books#create"
+get "/books/:id", to: "books#show", as: :book
 end
