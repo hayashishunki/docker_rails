fe08ac13ebfd040757ed143b89f783d4d4d86a55
hayashinoshun <s.y1014torimaru@gmail.com>
2022-07-30 20:58:51 +0900

    delete機能作成完了 controller, view, routh追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ post "/books", to: "books#create"
 get "/books/:id", to: "books#show", as: :book
 get "/books/:id/edit", to: "books#edit", as: :edit_book
 patch "/books/:id", to: "books#update"
+delete "/books/:id", to: "books#destroy"
+# resources :books
 end
