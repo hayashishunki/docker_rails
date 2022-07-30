39da8dcb7015a94f5b2764321effa284bf1c6035
hayashinoshun <s.y1014torimaru@gmail.com>
2022-07-31 00:16:00 +0900

    アンカーをlink_toに変更  WIP rails方式に変更中

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 get "/help", to: "home#help"
-get "/books", to: "books#index"
-get "/books/new", to: "books#new", as: :new_book
-post "/books", to: "books#create"
-get "/books/:id", to: "books#show", as: :book
-get "/books/:id/edit", to: "books#edit", as: :edit_book
-patch "/books/:id", to: "books#update"
-delete "/books/:id", to: "books#destroy"
-# resources :books
+# get "/books", to: "books#index"
+# get "/books/new", to: "books#new", as: :new_book
+# post "/books", to: "books#create"
+# get "/books/:id", to: "books#show", as: :book
+# get "/books/:id/edit", to: "books#edit", as: :edit_book
+# patch "/books/:id", to: "books#update"
+# delete "/books/:id", to: "books#destroy"
+resources :books
 end
